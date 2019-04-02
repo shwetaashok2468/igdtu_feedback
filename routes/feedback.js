a5b89a051ed7975b2c260078d3bd7c5e8c591a97
Tushar <sahtushar@gmail.com>
2019-04-02 23:24:24 +0530

    s

--- routes/feedback.js
+++ routes/feedback.js
@@ -1,97 +1,90 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-require('../models/faculty');
-const Faculty = mongoose.model('faculties');
+require("../models/faculty");
+const Faculty = mongoose.model("faculties");
 
-require('../models/users');
-const User = mongoose.model('users');
+require("../models/users");
+const User = mongoose.model("users");
 
-require('../models/facultyResponse');
-const FacultyResponse = mongoose.model('facultyResponse');
+require("../models/facultyResponse");
+const FacultyResponse = mongoose.model("facultyResponse");
 
-
-
-const {ensureAuthenticated} = require('../helpers/auth');
+const { ensureAuthenticated } = require("../helpers/auth");
 
 //getting the idea page
-router.get('/', ensureAuthenticated, (req, res) => {
+router.get("/", ensureAuthenticated, (req, res) => {
+  (async () => {
+    let isSubmitted = await FacultyResponse.find({
+      student_id: req.user.id
+    });
 
-    (async()=>{
+    console.log(isSubmitted);
 
-        let isSubmitted=await FacultyResponse.find({
-            student_id:req.user.id
-        })
-
-        console.log(isSubmitted);
-
-
-    if(isSubmitted.length===0) {
-        Faculty.find({
-            batch: req.user.batch
-        })
-            .then((faculty) => {
-                console.log(faculty);
-                res.render('feedback/index', {
-                    faculty: faculty[0].facultyList,
-                });
-            })
+    if (isSubmitted.length === 0) {
+      let faculty = await Faculty.findOne({
+        batch: req.user.batch
+      });
+      console.log(faculty);
+      res.render("feedback/index", {
+        faculty: faculty.facultyList
+      });
+    } else {
+      res.redirect("/feedback/success");
     }
-    else {
-        res.redirect('/feedback/success');
-    }
-
-    })();
+  })();
 });
 
-router.post('/submit_form', ensureAuthenticated, (req, res) => {
-    (async () => {
-        //getting user batch
-        let batch = await Faculty.find({
-            batch: req.user.batch
-        });
+router.post("/submit_form", ensureAuthenticated, (req, res) => {
+  (async () => {
+    //getting user batch
+    let batch = await Faculty.findOne({
+      batch: req.user.batch
+    });
 
-        //getting FacultyList for that batch
-        let facultyList = batch[0].facultyList;
-        let form_values = (req.body);
-        let response = [];
-        let facultyResponse = new FacultyResponse();
-        facultyResponse.student_id = req.user.id;
-        facultyResponse.batch=req.user.batch;
-        for (let j = 1; j <= facultyList.length; j++) {
-            let values = [];
-            for (let i = 0; i < Object.entries(form_values).length; i++) {
-                let x = "f" + j;
-                if ((Object.entries(form_values)[i][0]).search(x) > -1) {
-                    values.push(Object.entries(form_values)[i][1]);
-                }
-            }
-            let object = {};
-            console.log(facultyList[j - 1]._id);
-            object.faculty_id = facultyList[j - 1]._id;
-            object.criteria = values;
-            response.push(object);
-
-
+    //getting FacultyList for that batch
+    let facultyList = batch.facultyList;
+    let form_values = req.body;
+    let response = [];
+    let facultyResponse = new FacultyResponse();
+    facultyResponse.student_id = req.user.id;
+    facultyResponse.batch = req.user.batch;
+    for (let j = 1; j <= facultyList.length; j++) {
+      let values = [];
+      for (let i = 0; i < Object.entries(form_values).length; i++) {
+        let x = "f" + j;
+        if (Object.entries(form_values)[i][0].search(x) > -1) {
+          values.push(Object.entries(form_values)[i][1]);
         }
-        facultyResponse.response = response;
-        try {
-            await facultyResponse.save();
-            console.log("Saved");
-            res.redirect('/feedback/success')
-        }
-        catch (e) {
-            console.log(error);
-        }
-
-
-    })();
-
+      }
+      let object = {};
+      console.log(facultyList[j - 1]._id);
+      object.faculty_id = facultyList[j - 1]._id;
+      object.criteria = values;
+      response.push(object);
+    }
+    facultyResponse.response = response;
+    try {
+      await facultyResponse.save();
+      console.log("Saved");
+      res.redirect("/feedback/success");
+    } catch (e) {
+      console.log(error);
+    }
+  })();
 });
 
-router.get('/success',ensureAuthenticated,(req,res)=>{
-    res.render('feedback/success');
-})
+router.get("/success", ensureAuthenticated, (req, res) => {
+  (async () => {
+    let batch = await Faculty.findOne({
+      batch: req.user.batch
+    });
+
+    res.render("feedback/success",{
+        faculty: batch.facultyList
+    });
+  })();
+});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,221 +1,180 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 //Users model
-require('../models/users');
-const User = mongoose.model('users');
+require("../models/users");
+const User = mongoose.model("users");
 
 const passport = require("passport");
 const bcrypt = require("bcryptjs");
 
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
-router.get('/login', (req, res) => {
-
-    let message="";
-console.log(req.query.message);
-if(req.query.message){
-    message=req.query.message;
+router.get("/login", (req, res) => {
+  let message = "";
+  console.log(req.query.message);
+  if (req.query.message) {
+    message = req.query.message;
     console.log(message);
-}
+  }
 
-    console.log("login page");
-    res.render("users/login",{
-         message:message
-    });
-
+  console.log("login page");
+  res.render("users/login", {
+    message: message
+  });
 });
-router.get('/register', (req, res) => {
-
-
-    res.render("users/register", {
-        errors: [],
-        name: "",
-        email: "",
-        password: "",
-        password2: ""
-    });
-
+router.get("/register", (req, res) => {
+  res.render("users/register", {
+    errors: [],
+    name: "",
+    email: "",
+    password: "",
+    password2: ""
+  });
 });
 
-
-
-router.post('/register', (req, res) => {
-
+router.post("/register", (req, res) => {
+  (async () => {
     let errors = [];
     //password validations
     if (req.body.password !== req.body.password2) {
-        errors.push({text: "passwords do not match"});
-
+      errors.push({ text: "passwords do not match" });
     }
 
     if (req.body.password.length < 4) {
-        errors.push({text: "Password length is small"});
-
+      errors.push({ text: "Password length is small" });
     }
 
     if (errors.length > 0) {
-        res.render("users/register", {
+      res.render("users/register", {
+        errors: errors,
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        password2: req.body.password2
+      });
+    }
+
+    //if no errors regarding the validations
+    else {
+      //all validations passed
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        firstUser: false
+      }); //we will create a newUser variable
+
+      //now we have to check whether the email id exists or not
+      User.findOne({ email: newUser.email }).then(user => {
+        if (user && user.googleID.length > 0) {
+          errors.push({ text: "Use Google Login" });
+
+          res.render("users/register", {
             errors: errors,
             name: req.body.name,
             email: req.body.email,
             password: req.body.password,
             password2: req.body.password2
-        })
-    }
-
-    //if no errors regarding the validations
-    else {
-
-        //all validations passed
-        const newUser = new User({
+          });
+        } else if (user && user.googleID.length <= 0) {
+          errors.push({ text: "User already Exists" });
+          res.render("users/register", {
+            errors: errors,
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
-            firstUser:false
+            password: req.body.password
+          });
 
-        }); //we will create a newUser variable
+          console.log("error_msg", error_msg);
+        } else {
+          bcrypt.genSalt(10, (err, salt) => {
+            //to generate salt for length upto 10 to hash the password
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              //these two lines is used to convert entered password to hash
 
-        //now we have to check whether the email id exists or not
-        User.findOne({email:newUser.email})
-            .then((user)=>{
-                if(user && user.googleID.length>0)
-                {
-                    errors.push({text: "Use Google Login"})
-
-                    res.render("users/register",{
-                        errors:errors,
-                        name: req.body.name,
-                        email: req.body.email,
-                        password: req.body.password,
-                        password2:req.body.password2
-                    });
-                }
-                else if(user && user.googleID.length<=0){
-
-                    errors.push({text:"User already Exists"});
-                    res.render("users/register",{
-                        errors:errors,
-                        name: req.body.name,
-                        email: req.body.email,
-                        password: req.body.password,
-                    });
-
-                    console.log("error_msg",error_msg);
-                }
-                else{
-                    bcrypt.genSalt(10, (err, salt) => {  //to generate salt for length upto 10 to hash the password
-                        bcrypt.hash(newUser.password, salt, (err, hash) => { //these two lines is used to convert entered password to hash
-
-                            newUser.password = hash;
-                            newUser.save()
-                                .then(() => {
-                                    req.flash("success_msg", "Registration Successful");
-                                    res.redirect("/users/login");
-                                })
-                        })
-                    })
-                }
-
-            })
-
+              newUser.password = hash;
+              newUser.save().then(() => {
+                req.flash("success_msg", "Registration Successful");
+                res.redirect("/users/login");
+              });
+            });
+          });
+        }
+      });
     }
-
-
-
-
+  })();
 });
-router.get('/setPassword', (req, res) => {
-    console.log("set Password");
-    res.render('users/setPassword', {
-        errors: []
-    })
-
-})
-router.post('/login', (req,res,next)=>{
-
-
-    passport.authenticate('local',{
-        successRedirect:'/feedback',
-        failureRedirect:'/users/login?message=Wrong password or user does not exists',
-        failureFlash:true,
-
-
-    })(req,res,next);
-
-
-
-
-
+router.get("/setPassword", (req, res) => {
+  console.log("set Password");
+  res.render("users/setPassword", {
+    errors: []
+  });
+});
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/feedback",
+    failureRedirect:
+      "/users/login?message=Wrong password or user does not exists",
+    failureFlash: true
+  })(req, res, next);
 });
 
-router.post('/setPassword', (req, res) => {
-    let errors = [];
-    if (req.body.password !== req.body.password2) {
-        errors.push({text: "Password does not match"});
-    }
-    if (req.body.password.length < 4) {
-        errors.push({text: "Password length is less than 4"})
-    }
-    if(req.body.batch==="select")
-    {
-        errors.push({text: "Select Your batch first"});
-    }
+router.post("/setPassword", (req, res) => {
+  let errors = [];
+  if (req.body.password !== req.body.password2) {
+    errors.push({ text: "Password does not match" });
+  }
+  if (req.body.password.length < 4) {
+    errors.push({ text: "Password length is less than 4" });
+  }
+  if (req.body.batch === "select") {
+    errors.push({ text: "Select Your batch first" });
+  }
 
-    if (errors.length > 0) {
-        res.render('users/setPassword', {
-            errors: errors
+  if (errors.length > 0) {
+    res.render("users/setPassword", {
+      errors: errors
+    });
+  } else {
+    User.findOne({
+      email: req.user.email
+    }).then(user => {
+      bcrypt.genSalt(10, (err, salt) => {
+        //to generate salt for length upto 10 to hash the password
+        bcrypt.hash(req.body.password, salt, (err, hash) => {
+          //these two lines is used to convert entered password to hash
+
+          user.password = hash;
+          user.batch = req.body.batch;
+          user.firstUser = false;
+
+          user.save().then(user => {
+            req.flash("success_msg", "Password Set Successfully");
+            console.log(user.firstUser);
+            res.redirect("/feedback");
+          });
         });
-    }
-    else {
-
-        User.findOne({
-            email:req.user.email
-        })
-            .then((user)=>{
-            bcrypt.genSalt(10, (err, salt) => {  //to generate salt for length upto 10 to hash the password
-                bcrypt.hash(req.body.password, salt, (err, hash) => { //these two lines is used to convert entered password to hash
-
-                    user.password = hash;
-                    user.batch=req.body.batch;
-                    user.firstUser = false;
-
-                    user.save()
-                        .then((user) => {
-                            req.flash("success_msg", "Password Set Successfully");
-                            console.log(user.firstUser);
-                            res.redirect("/feedback");
-                        })
-                })
-            })
-        })
-
-
-
-    }
-
-
+      });
+    });
+  }
 });
 
-router.get('/logout',(req,res)=>{
-    req.logout();
-    req.flash("success_msg","logout Successfully");
-    res.redirect('/users/login/');
-
+router.get("/logout", (req, res) => {
+  req.logout();
+  req.flash("success_msg", "logout Successfully");
+  res.redirect("/users/login/");
 });
 
-router.get('/admin', (req,res)=>{
-    res.render('users/admin');
+router.get("/admin", (req, res) => {
+  res.render("users/admin");
 });
 
-router.get('/add_batch', (req,res)=>
-{
-    res.render('users/add_batch');
+router.get("/add_batch", (req, res) => {
+  res.render("users/add_batch");
 });
 
-
-router.get('/add_faculty', (req,res)=>
-{
-    res.render('users/add_faculty');
+router.get("/add_faculty", (req, res) => {
+  res.render("users/add_faculty");
 });
 module.exports = router;
