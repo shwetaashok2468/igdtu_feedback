12e7f01de4c6274cce05c9526ecfd9f455af51ad
Tushar <sahtushar@gmail.com>
2019-04-06 00:47:11 +0530

    Added graph functionality

--- routes/feedback.js
+++ routes/feedback.js
@@ -31,7 +31,7 @@ router.get("/", ensureAuthenticated, (req, res) => {
         faculty: faculty.facultyList
       });
     } else {
-      res.redirect("/feedback/success");
+      res.redirect("/feedback/graph");
     }
   })();
 });
@@ -68,21 +68,128 @@ router.post("/submit_form", ensureAuthenticated, (req, res) => {
     try {
       await facultyResponse.save();
       console.log("Saved");
-      res.redirect("/feedback/success");
+      res.redirect("/feedback/graph");
     } catch (e) {
       console.log(error);
     }
   })();
 });
 
-router.get("/success", ensureAuthenticated, (req, res) => {
+router.get("/graph", ensureAuthenticated, (req, res) => {
   (async () => {
+    //first graph - each teacher vs total sum of criteria
+    // get total sum of each according to batch
+
     let batch = await Faculty.findOne({
       batch: req.user.batch
     });
 
-    res.render("feedback/success",{
-        faculty: batch.facultyList
+    let responses = await FacultyResponse.find({
+      batch: batch.batch
+    });
+
+    console.log(JSON.stringify(responses));
+    let facultyLength=responses[0].response.length;
+    let performance=[];
+    for(let i=0;i<facultyLength;i++){
+      let facultyPerformance=0;
+      let facultyId;
+      for(let j=0;j<responses.length;j++){
+        try {
+
+          console.log(responses[j].response[i].criteria);
+            let current_sum=(responses[j].response[i].criteria).reduce((acc,curr)=>{
+              return (Number(acc)+Number(curr));
+            });
+            console.log(current_sum);
+            facultyPerformance+=current_sum;
+            facultyId=responses[j].response[i].faculty_id;
+            console.log(responses[j].response[i].faculty_id);
+        }
+        catch (e) {
+            
+        }
+      }
+      let faculty=await Faculty.findOne({
+          batch:req.user.batch
+
+      });
+      let facultyName;
+      faculty.facultyList.map((val)=>{
+        if(val.id.match(facultyId)){
+          facultyName=val.name;
+        }
+      });
+      console.log(facultyName);
+
+      console.log(facultyPerformance);
+        performance.push({facultyName:facultyName, performance:facultyPerformance});
+
+    }
+      facultyLength=responses[0].response.length;
+      let bestPerformance=[];
+      for(let i=0;i<facultyLength;i++){
+          let facultyPerformance=0;
+          let facultyId;
+          for(let j=0;j<responses.length;j++){
+              try {
+
+                  let a=responses[j].response[i].criteria;
+                  let result = a.map(function (x) {
+                      return parseInt(x, 10);
+                  });
+                  console.log(result);
+                  let count = result.reduce(function (acc, curr) {
+                      if (typeof acc[curr] ==='undefined') {
+                          acc[curr] = 1;
+                      } else {
+                          acc[curr] += 1;
+                      }
+
+                      return acc;
+                  }, {});
+                  let current_sum=0;
+                  let count4=count[4]?count[4]:0;
+                  let count5=count[5]?count[5]:0;
+
+
+                       current_sum = count4 + count5;
+
+                      console.log(current_sum);
+                      facultyPerformance += current_sum;
+                      facultyId = responses[j].response[i].faculty_id;
+                      console.log(responses[j].response[i].faculty_id);
+
+              }
+              catch (e) {
+
+              }
+          }
+          let faculty=await Faculty.findOne({
+              batch:req.user.batch
+
+          });
+          let facultyName;
+          faculty.facultyList.map((val)=>{
+              if(val.id.match(facultyId)){
+                  facultyName=val.name;
+              }
+          });
+          bestPerformance.push({facultyName:facultyName, performance:facultyPerformance});
+
+      }
+
+    /*let labels=[];
+    performance.map(val=>{return labels.push(val.facultyId)});
+
+    let data=[];
+    performance.map(val=>{ return data.push(val.performance)});*/
+
+
+    console.log(JSON.stringify(performance));
+    res.render("feedback/graph",{
+      performance:performance,
+        bestPerformance:bestPerformance
     });
   })();
 });
